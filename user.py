dec952e58205c73f8be5d68e7ca3389fd9267264
Emilio Lopez <eil11@case.edu>
2016-10-14 20:21:32 -0400

    Editing the indentations of the user class. Also a test of commissions inGitHub.

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 class User:
     def __init__(self, name, sign, meme):
-    self.name = neame
-    self.sign = sign
-    self.memes = meme
+        self.name = neame
+        self.sign = sign
+        self.memes = meme
