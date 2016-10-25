97f7a76eeef7a0fd77837e11bde5ae49d41e4ad4
Jacob Rosales Chase <coby@case.edu>
2016-10-24 23:14:25 -0400

    Improve test module

--- test.py
+++ test.py
@@ -2,29 +2,30 @@ import Sheets
 from Sheets import User_Sheet
 from User import User
 
-usrsheet = Sheets.User_Sheet("Lockonia_user_sheet")
+usrsheet = User_Sheet("Lockonia_user_sheet", 0)
 
-sheet = Sheets.get_sheet("Lockonia_user_sheet")
+users = [
+    ["Harry", 'hxp1', 'urawizard', 'h.potter@hogwarts.edu'],
+    ["Barry", 'blt3', 'no', 'barry@jetpackfarm.edu'],
+    ["Bork", 'bor6', 'literallybork', 'bork@doge.net'],
+    ["Mr. Bantuski", 'axb34', 'tusssshy', 'none'],
+    ["Marvin", 'mad838', 'ladDiggity', 'fish@glub.sea'],
+    ["Nobody", 'nbd000', '0', 'nobody@somewhere.japan'],
+    ["Alan", 'axb34', 'menslenissimo', 'terrible@case.edu'],
+    ["Terry", 'ter9', 'mansuplicatoriness', 'cruz@aiders.org']
+    ]
+
+fml = [users[0], users[4], users[len(users) - 1]]  #first middle last
 
 def populate():
-    user1 = User("Harry", 'hxp1', 'urawizard', 'h.potter@hogwarts.edu')
-    user2 = User("Barry", 'blt3', 'no', 'barry@jetpackfarm.edu')
-    user3 = User("Bork", 'bor6', 'literallybork', 'bork@doge.net')
-    user4 = User("Mr. Bantuski", 'axb34', 'tusssshy', 'none')
-    user5 = User("Marvin", 'mad838', 'ladDiggity', 'fish@glub.sea')
-    user6 = User("Nobody", 'nbd000', '0', 'nobody@somewhere.japan')
-    user7 = User("Alan", 'axb34', 'menslenissimo', 'terrible@case.edu')
-    user8 = User("Terry", 'ter9', 'mansuplicatoriness', 'cruz@aiders.org')
+    testadd(users)
 
-    usrsheet.add_user(user1)
-    usrsheet.add_user(user2)
-    usrsheet.add_user(user3)
-    usrsheet.add_user(user4)
-    usrsheet.add_user(user5)
-    usrsheet.add_user(user6)
-    usrsheet.add_user(user7)
-    usrsheet.add_user(user8)
+def testadd(to_add):
+    for user in to_add:
+        print("add %s"%str(user))
+        print(usrsheet.add_user(User.from_array(user)))
 
-print(usrsheet.remove_user("Harry"))
-print(usrsheet.remove_user("Marvin"))
-print(usrsheet.remove_user("Terry"))
+def testdelete(to_delete):
+    for item in to_delete:
+        print("delete %s"%str(item))
+        print(usrsheet.remove_user(item[0]))
