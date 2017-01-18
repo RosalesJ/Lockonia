d4ab8755dbbf86bbb67ed6e8276851ae1baa59d8
Emilio <eil11@case.edu>
2017-01-17 21:30:43 -0500

    Implements the tracking of active cameras with Sheets. Uses magic numbers to indicate state of the camera. Contains typos and organization errors.

--- Admin.py
+++ Admin.py
@@ -1,14 +1,14 @@
 import Event
 import User
 import Sheets
-import configparser
 
 users = Sheets.User_Sheet('Lockonia_User_Sheet', 0)
+cameras = Sheets.Sheet('Lockonia_Cameras_Sheet', 0)
 
 #User_Sheet Interactions
 
 def create_new_user():
-''' Accept multiple inputs, create a User, and add it to the User_Sheet. '''
+    ''' Accept multiple inputs, create a User, and add it to the User_Sheet. '''
 
     print('Creating new user.')
     name = input('Enter user name: ')
@@ -25,7 +25,7 @@ def create_new_user():
         return False
 
 def remove_user():
-''' Remove a user from the user sheet. '''
+    ''' Remove a user from the user sheet. '''
 
     print('Removing user')
     userID = input('Enter Case ID (without trailing whitespace) OR tap card: ')
@@ -40,74 +40,61 @@ def remove_user():
 
 # Cameras Interactions
 
-def camera_init(disable):
-''' Helper. Setups the access of the Cameras.ini to search for the correct options
-    based on the command given. '''
-
-    config = configparser.ConfigParser()
-    config.read('.\\Cameras.ini')
-    camera_count = config.getint('Cameras', 'cameras')
-
-    # Disable branch: When the admin intends to disable a camera
-    if disable:
-        # Verifies that the limited slots on the camera shelf are not all vacant
-        if camera_count > 0:
-            cameras_list = config.items('Cameras')
-            options_list = []
-            tracker = 1
-            for value in cameras_list:
-                if value[1] == 2:
-                    options_list.append('cam' + tracker)
-                tracker = tracker + 1
-            tracker = 1
-            return True
-        else:
-            print("Disabling failed. No cameras to disable.")
-            return False
-
-    # Enable branch: When the admin intends to enable a camera
-    else:
-        # Verifies that the limited slots on the camera shelf are not all filled
-        if camera_count < 4:
-            cameras_list = config.items('Cameras')
-            options_list = []
-            tracker = 1
-            for value in cameras_list:
-                if value[1] == 0:
-                    options_list.append('cam' + tracker)
-                tracker = tracker + 1
-            tracker = 1
-            return True
-        else:
-            print('Enabling failed. No more camera slots available.')
-            return False
-
 def disable_camera():
-''' Disables a camera slot, so that the case cannot be withdrawn '''
+    ''' Disables a camera slot, so that the case cannot be withdrawn '''
+    print('Disabling a camera.')
+    camera_disable = input('Select a camera. eg: 3 : ')
 
-    if camera_init(True):    # 'Selects' the disable branch of camera_init
-        cam_selection = input('Select which camera to remove')
+    # Validates the camera selection
+    try:
+        if int(cameras.acell('B' + camera_disable).value) > 0:
+            if int(camera_disable) <= 4 and int(camera_disable) > 0:
+                if int(cameras.acell('B' + camera_disable).value) == 2:
+                    cameras.update_acell('B' + camera_disable, '0')
+                    cameras.update_acell('B5', int(cameras.acell('B5').value) - 1)
+                    print("Disable completed successfully. There are " + cameras.acell('B5').value + 'remaining cameras.\n')
+                else:
+                    print('Cannot disable that camera. It is either already disabled or currently checked out.')
 
-        #TODO Verify that the options_list here will work. Expected: Will not work
 
-        for camera in options_list:
-            if cam_selection == camera:
-                config.set('Cameras', cam_selection, '0')
-                config.set('Camera_Count', 'cameras', str((config.getint('Cameras', 'cameras') - 1)))
+            else:
+                print('Disabling failed. Invalid values. Is your input between (inclusive) 1 and 4 ? \n')
+        else:
+            print('No disabled cameras to enable.')
+
+    except ValueError:
+        print('Disabling failed. Invalid values. Please enter only a number. ')
+
+    except:
+        print('Unknown error. Verify that your input is only a number.')
 
 def enable_camera():
-''' Enables a camera slot, so that the case can be withdrawn. '''
+    ''' Enables a camera slot, so that the case can be withdrawn. '''
+    print('Enabling a camera.')
+    camera_enable = input('Select a camera. eg: 3 : ')
 
-    if camera_init(False):    # 'Selects' the enable branch of camera_init
-        cam_selection = input('Select which camera to enable')
+    # Validates the camera selection
+    try:
+        if int(cameras.acell('B5').value) < 4:
+            if int(camera_enable) < 4 and int(camera_enable) > 0:
+                if int(cameras.acell('B' + camera_enable).value) == 2:
+                    cameras.update_acell('B' + camera_enable, '0')
+                    cameras.update_acell('B5', int(cameras.acell('B5').value) + 1)
+                    print("Disable completed successfully. There are " + cameras.acell('B5').value + 'remaining cameras.\n')
+                else:
+                    print('Cannot disable that camera. It is either already disabled or currently checked out.')
 
-        #TODO Verify that the options_list here will work. Expected: Will not work
 
-        for camera in options_list:
-            if cam_selection == camera:
-                config.set('Cameras', cam_selection, '2')
-                config.set('Camera_Count', 'cameras', str((config.getint('Cameras', 'cameras') + 1)))
+            else:
+                print('Disabling failed. Invalid values. Is your input between (inclusive) 1 and 4 ? \n')
+        else:
+            print('No disabled cameras to enable. ')
 
+    except ValueError:
+        print('Disabling failed. Invalid values. Please enter only a number. ')
+
+    except:
+        print('Unknown error. Verify that your input is only a number.')
 
 ''' Main Program '''
 
@@ -117,16 +104,16 @@ print("#############################\n")
 
 print("Type 'User' for user management")
 print("Type 'Camera' for camera case management")
-action = input('User or Camera')
+action = input('User or Camera: ' )
+
+command_not_entered = True
 
 if action == 'User':
     print("Type 'create' to create a new user.")
     print("Type 'remove' to remove a user.")
 
-    command_not_entered = True
-
     while command_not_entered:
-        command = input("Only enter 'create' or 'remove'. ")
+        command = input("Only enter 'create' or 'remove': ")
 
         if command == 'create':
             command_not_entered = False
@@ -141,6 +128,22 @@ if action == 'User':
         remove_user()
 
 elif action == 'Camera':
+    print("Type 'disable' to disable a camera")
+    print("Type 'enable' to enable a camera")
+
+    while command_not_entered:
+        command = input("Only enter 'enable' or 'disable': ")
+        if command == 'enable':
+            command_not_entered = False
+        elif command == 'disable':
+            command_not_entered = False
+        else:
+            print("Only 'enable' or 'disable' are valid commands!")
+
+    if command == 'enable':
+        enable_camera()
+    else:
+        disable_camera()
 
 else:
     print("Error. Only enter 'Camera' or 'User'")
