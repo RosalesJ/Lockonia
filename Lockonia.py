dd9ed23cd836a76171a828cf6294ab13fcadf46c
Jacob Rosales Chase <coby@case.edu>
2017-01-18 19:58:57 -0500

    Modify the field to reflect card input

--- Lockonia.py
+++ Lockonia.py
@@ -2,6 +2,7 @@ import LockController
 from Sheets import User_Sheet
 from Sheets import Entry_Sheet
 from itertools import chain
+import time
 
 from kivy.app import App
 from kivy.lang import Builder
@@ -30,8 +31,9 @@ class Gradient(object):
         return texture
 
 class StartScreen(Widget):
-    def on_card_tap(instance, value):
+    def on_card_tap(instance, value, label_thing):
         print(value.text)
+        label_thing.text = "Welcome " + value.text
         value.select_all()
         value.delete_selection()
         value.focus = True
