0b81065b66d2e8e008c142f4a56e7396738f535f
Jacob Rosales Chase <coby@case.edu>
2017-01-21 03:07:14 -0500

    Add a basic work flow to the program with multiple more screens with appropriate names. Change a couple of naming conventions. Remove the color specifications from the KV file for the sake of a global variable stored in the .py file and imported to the kv file.

--- Lockonia.py
+++ Lockonia.py
@@ -14,11 +14,19 @@ from kivy.graphics.texture import Texture
 from kivy.uix.behaviors.focus import FocusBehavior
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
-UserSheet = UserSheet("Lockonia_User_Sheet", 0)
-EntrySheet = EntrySheet("Lockonia_Log_Sheet", 0)
-CameraSheet = Sheet("Lockonia_Cameras_Sheet", 0)
+users = UserSheet("Lockonia_User_Sheet", 0)
+log = EntrySheet("Lockonia_Log_Sheet", 0)
+cameras = Sheet("Lockonia_Cameras_Sheet", 0)
 
-CurrentUser = None
+blue  = (0.31640625,0.7098039216,0.8549019608,1)
+white = (1, 1, 1, 1)
+grey1 = (0.780, 0.780, 0.780, 1)
+grey2 = (0.800, 0.800, 0.800, 1)
+grey3 = (0.95, 0.95, 0.95, 1)
+text_color = (0.25, 0.25, 0.25, 1)
+
+
+current_user = None
 
 class Gradient(object):
     @staticmethod
@@ -39,11 +47,10 @@ class Gradient(object):
 
 class CardReader(TextInput):
     def on_card_tap(instance, value):
-        global CurrentUser
-        CurrentUser = UserSheet.get_user(value.text)
-        print(value.text)
+        global current_user
+        current_user = users.get_user(value.text)
         value.text = ''
-        if CurrentUser:
+        if current_user:
             Clock.schedule_once(value.parent.welcome_user, 0.2)
         else:
             Clock.schedule_once(value.parent.invalid_user, 0.2)
@@ -58,20 +65,52 @@ class StartScreen(Screen):
     def focus_card_reader(self, card_reader):
         card_reader.focus = True
 
-class WelcomeScreen(Screen):
-    def on_touch_down(self, *args):
-        self.manager.current = "start"
+class HomeScreen(Screen):
+    def goto_checkin(self):
+        time.sleep(0.5)
+        self.manager.current = "checkin"
+
+    def goto_checkout(self):
+        time.sleep(0.5)
+        self.manager.current = "checkout"
 
 class InvalidUserScreen(Screen):
     def on_touch_down(self, *args):
         self.manager.current = "start"
 
+class CheckoutScreen(Screen):
+    def on_touch_down(self, *args):
+        Clock.schedule_once(self.done, 0.1)
+
+    def done(self, *args):
+        self.manager.current = "confirmation"
+
+    def checkout(self, item):
+        print("I checked out", item)
+
+class CheckinScreen(Screen):
+    def on_touch_down(self, *args):
+        Clock.schedule_once(self.done, 0.1)
+
+    def done(self, *args):
+        self.manager.current = "confirmation"
+
+    def checkout(self, item):
+        print("I checked in", item)
+
+class ConfirmationScreen(Screen):
+     def on_touch_down(self, *args):
+         self.manager.current = "start"
+
 class LockoniaApp(App):
     def build(self):
         root = ScreenManager(transition=FadeTransition())
         root.add_widget(StartScreen())
-        root.add_widget(WelcomeScreen())
+        root.add_widget(HomeScreen())
         root.add_widget(InvalidUserScreen())
+        root.add_widget(CheckoutScreen())
+        root.add_widget(CheckinScreen())
+        root.add_widget(ConfirmationScreen())
         return root
 
 Builder.load_file("Lockonia.kv")
