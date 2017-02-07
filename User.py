270613a5c44a60e581cc4e490ea7b201d577fffe
Emilio <eil11@case.edu>
2017-02-07 00:44:51 -0500

    Edit user to account for UserSheet's getuser method.

--- User.py
+++ User.py
@@ -1,17 +1,17 @@
 # Represents a user
 class User:
-    def __init__(self, name, caseID, cardID, email, allowed_cameras):
+    def __init__(self, name, caseID, cardID, email, allowed_cameras, cameras):
         self.name = name
         self.caseID = caseID
         self.cardID = cardID
         self.email = email
         self.allowed_cameras = allowed_cameras
-        self.cameras = []
+        self.cameras = cameras.split(',')
 
     # instantiates a User from an array of 4 items
     @classmethod
     def from_array(cls, arr):
-        return cls(arr[0], arr[1], arr[2], arr[3])
+        return cls(arr[0], arr[1], arr[2], arr[3], arr[4])
 
 
     def to_array(self):
@@ -21,7 +21,7 @@ class User:
         arr.append(self.caseID)
         arr.append(self.cardID)
         arr.append(self.email)
-        arr.append(join(self.cameras()))
+        arr.append(','.join(self.cameras()))
 
         return arr
 
